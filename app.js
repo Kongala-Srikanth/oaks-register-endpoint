bdbe7f612d49e419fcf23d6af898aa0bcc4296c3
srikanth <kongala.srikanth44@gmail.com>
2025-01-29 21:23:05 +0530

    Added Error Handling

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ app.use(express.json());
 app.post("/register", (req, res) => {
     const { name, email, password } = req.body;
 
-    if (!name || !email || !password) {
-        return res.status(400).json({ success: false, message: "All fields are required" });
+    try{
+        if (!name || !email || !password) {
+            return res.status(400).json({message: "All fields are required" });
+        }
+    
+        res.status(201).json({message: "User registered successfully"});
+    } catch (e) {
+        res.status(500).json({message: "Something went wrong. Please try again"})
     }
 
-    res.status(201).json({ success: true, message: "User registered successfully", data: { name, email } });
+    
 });
 
 app.listen(3000, () => {
